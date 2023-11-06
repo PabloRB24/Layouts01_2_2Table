035c2a518764ff22fdc95d4d563f358b75ea5163
PabloRB24 <pabrod138@alu.centoafuera.es>
2023-11-06 12:30:57 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Layouts01_2_2"
+include(":app")
+ 
